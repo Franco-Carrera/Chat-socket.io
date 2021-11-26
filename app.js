aac3762feaca5b28f755607f845eebb36b6db3cc
Franco-Carrera <carrerafranco825@gmail.com>
2021-11-25 22:20:32 -0300

    first-commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+  console.log("Listening to port: " + PORT);
+});
+app.use(express.static(__dirname + "/public"));
+const io = new Server(server);
+let messages = [];
+
+io.on("connection", (socket) => {
+  console.log("Cliente conectado");
+
+  // socket.emit("messagelog", messages)
+
+  socket.emit("welcome", "Bienvenido a mi Socket grandioso");
+
+  socket.on("message", (data) => {
+    messages.push({ data });
+    io.emit("messagelog", messages);
+  });
+});
+// aquí welcome lo recibe index.js
